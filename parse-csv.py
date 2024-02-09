9c2ab3c4657d0fc04875573b9482713a456faa69
pscally1005 <scallypr@gmail.com>
2024-02-09 10:49:41 -0500

    python
    wrote a python script to take the csv files i downloaded, split them into 2 files, and remove the original.  will loop through and update all relevant files, while leaving the others alone.  just have to run the script and refresh the page to update ingredients and nutrition facts

---
+++ parse-csv.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import os
+import glob
+import csv
+
+# path to csv files
+path = r"C:\Users\mets1\Documents\website\_data\*.csv"
+
+# loop through all the files
+for fname in glob.glob(path):
+
+    # only use files that haven't been parsed yet
+    if(fname[-10:] != "-facts.csv" and fname[-8:] != "-ing.csv"):
+        with open(fname, 'r') as fin:
+            data = fin.read().splitlines(True)
+
+        # remove first and last few rows from file
+        temp = fname[:-4] + "-temp.csv"
+        with open(temp, 'w') as fout:
+            fout.writelines(data[6:-7])
+
+        # get row of blank (separation between 2 tables)
+        blank = -1
+        with open(temp, newline='') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            i = 0
+            for row in spamreader:
+                if(len(row) == 0):
+                    blank = i
+                    # print(', '.join(row))
+                    break
+                i += 1
+        
+        #  now, read from temp file
+        with open(temp, 'r') as fin:
+            data = fin.read().splitlines(True)
+
+        # put first half in ingredients file
+        ing = fname[:-4] + "-ing.csv"
+        with open(ing, 'w') as fout:
+            fout.writelines(data[:blank])
+
+        # put second half in facts file
+        facts = fname[:-4] + "-facts.csv"
+        with open(facts, 'w') as fout:
+            fout.writelines(data[blank+1:])
+
+        # delete original and temp file
+        os.remove(fname)
+        os.remove(temp)
