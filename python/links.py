7ad0509c607c83ee86e65cf40c7f619e4c2da447
pscally1005 <scallypr@gmail.com>
2026-01-25 14:46:45 -0500

    couple straggler links (archive & draft)

--- python/links.py
+++ python/links.py
@@ -3,7 +3,7 @@ import re
 from html import escape, unescape
 from bs4 import BeautifulSoup, NavigableString
 
-POSTS_DIR = r"C:\Users\mets1\Documents\website\_posts\delete"
+POSTS_DIR = r"C:\Users\mets1\Documents\website\_posts\misc"
 # POSTS_DIR = r"C:\Users\mets1\Documents\GitHub\pscally1005.github.io\_posts\delete"
 
 LINKS = {
